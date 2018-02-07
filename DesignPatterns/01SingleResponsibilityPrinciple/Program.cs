7be4aa58fd57e9b8b0b211b2332120935243c21c
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-02-07 14:04:06 -0500

    Solid: Single Responsibility Principle

---
+++ DesignPatterns/01SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    //Definition: A class should have only one reason to change.
+    //Journal class has the responsiblity to add and remove entries only
+    //Other resposibilities such as saving the file to disk should be given to other class
+
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        public static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal j, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, j.ToString());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("I cried today");
+            j.AddEntry("I ate a bug");
+
+            Console.WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"c:\temp\journal.txt";
+            p.SaveToFile(j, filename, true);
+            Process.Start(filename);
+        }
+    }
+}
