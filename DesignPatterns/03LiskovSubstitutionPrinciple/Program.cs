18004bd5d5bd59325a3cd9977d9da443b9e08e19
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-02-08 08:40:16 -0500

    Solid: Liskov Substitution Principle

---
+++ DesignPatterns/03LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitutionPrinciple
+{
+    //Definition: We should be able to subsitute a base type for a sub type.
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"Width: {Width}, Height: {Height}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public override int Width
+        {
+            set { base.Width = base.Height = value; }
+        }
+
+        public override int Height
+        {  
+            set  { base.Height = base.Width = value; }
+        }
+    }
+
+
+    class Program
+    {
+        public static int Area(Rectangle r) => r.Width * r.Height;
+
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle(2,3);
+            Console.WriteLine($"{rc} has area {Area(rc)}");
+
+            Rectangle sq = new Square();
+            sq.Width = 4;
+            Console.WriteLine($"{sq} has area {Area(sq)}");
+        }
+    }
+}
