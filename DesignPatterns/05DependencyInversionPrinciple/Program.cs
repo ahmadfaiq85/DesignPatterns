26fd45b1558c274a8e8cddaef3ee07a1bd528358
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-02-08 12:47:59 -0500

    Solid: Dependency Inversion Principle

---
+++ DesignPatterns/05DependencyInversionPrinciple/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionPrinciple
+{
+    //Definition: High level parts of the system should not depend on low level parts of the system,
+    //instead they should depend on abstraction
+
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+    public enum Relationship
+    {
+        Parent,
+        Child,
+        Sibling
+    }
+
+    public interface IRelationshipBrowser
+    {
+        IEnumerable<Person> FindAllChildrenOf(string name);
+    }
+
+    public class Relationships : IRelationshipBrowser
+    {
+        public IEnumerable<Person> FindAllChildrenOf(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Research
+    {
+        public Research(IRelationshipBrowser browser)
+        {
+            foreach (var p in browser.FindAllChildrenOf("John"))
+                Console.WriteLine($"John has child {p.Name}");
+        } 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var relationships = new Relationships();
+            var research = new Research(relationships);
+        }
+    }
+}
